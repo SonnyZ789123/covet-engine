6e23a237525f15caa10d12a229a74193e2cdb5e2
SonnyZ789123 <yoran.mertens@telenet.be>
2025-12-29 14:27:11 +0100

    Remove AStar

--- src/main/gov/nasa/jpf/jdart/constraints/astar/CostFunction.java
+++
@@ -1,5 +0,0 @@
-package gov.nasa.jpf.jdart.constraints.astar;
-
-public interface CostFunction<T extends GraphNode> {
-    double computeCost(T from, T to);
-}
--- src/main/gov/nasa/jpf/jdart/constraints/astar/GraphNode.java
+++
@@ -1,5 +0,0 @@
-package gov.nasa.jpf.jdart.constraints.astar;
-
-public interface GraphNode {
-    String getId();
-}
