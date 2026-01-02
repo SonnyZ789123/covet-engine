d782aef495bfc9093c5336aaf233556aee680b76
SonnyZ789123 <yoran.mertens@telenet.be>
2026-01-02 10:31:37 +0100

    Pass MethodInfo to findNext

--- src/main/gov/nasa/jpf/jdart/constraints/ExplorationStrategy.java
+++ src/main/gov/nasa/jpf/jdart/constraints/ExplorationStrategy.java
@@ -1,9 +1,10 @@
 package gov.nasa.jpf.jdart.constraints;
 
 import gov.nasa.jpf.constraints.api.Valuation;
+import gov.nasa.jpf.vm.MethodInfo;
 
 public interface ExplorationStrategy {
 
-    Valuation findNext(InternalConstraintsTree ctx);
+    Valuation findNext(InternalConstraintsTree ctx, MethodInfo methodInfo);
 
 }
