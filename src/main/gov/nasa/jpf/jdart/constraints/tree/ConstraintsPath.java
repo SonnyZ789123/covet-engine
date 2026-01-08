2ed8ea9c731200954859fb4d16fe58b7ed6d9355
SonnyZ789123 <yoran.mertens@telenet.be>
2026-01-08 20:38:28 +0100

    Move some logic into ConstraintsPath

---
+++ src/main/gov/nasa/jpf/jdart/constraints/tree/ConstraintsPath.java
@@ -0,0 +1,116 @@
+package gov.nasa.jpf.jdart.constraints.tree;
+
+import gov.nasa.jpf.JPF;
+import gov.nasa.jpf.constraints.api.ConstraintSolver;
+import gov.nasa.jpf.constraints.api.Expression;
+import gov.nasa.jpf.constraints.api.SolverContext;
+import gov.nasa.jpf.constraints.api.Valuation;
+import gov.nasa.jpf.util.JPFLogger;
+import gov.nasa.jpf.util.Predicate;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class ConstraintsPath {
+    private final JPFLogger logger = JPF.getLogger("jdart");
+
+    private final JPFLogger debugLogger = JPF.getLogger("jdart.debug");
+
+    /** the expected path (list of branch indexes) through the constraints tree */
+    private final ArrayList<Integer> expectedPath = new ArrayList<>();
+
+    /** the solver context used to check path condition satisfiability */
+    private final SolverContext solverCtx;
+
+    public ConstraintsPath(SolverContext solverCtx) {
+        this.solverCtx = solverCtx;
+    }
+
+    public ArrayList<Integer> getExpectedPath() {
+        return new ArrayList<>(expectedPath);
+    }
+
+    public Node backtrack(Node startNode, Predicate<Node> stopCondition) {
+        debugLogger.finest("[backtrack] from expectedPath=" + expectedPath);
+        if (startNode == null)
+            return null;
+
+        Node currentNode = startNode;
+        while (!stopCondition.isTrue(currentNode)) {
+            currentNode = currentNode.getParent();
+
+            if (currentNode == null) {
+                debugLogger.finest("[backtrack] reached root parent -> stop");
+                break;
+            }
+
+            popExpectedPath();
+        }
+
+        debugLogger.finest("[bracktrack] new expectedPath=" + expectedPath);
+        return currentNode;
+    }
+
+    public void emptyExpectedPath() {
+        while (!expectedPath.isEmpty()) {
+            popExpectedPath();
+        }
+    }
+
+    public void popExpectedPath() {
+        solverCtx.pop();
+        int removed = expectedPath.remove(expectedPath.size() - 1);
+        debugLogger.finest("[popExpectedPath] removed branch " + removed);
+    }
+
+    public void extendExpectedPath(DecisionData decisionData, int branchIndex) {
+        Expression<Boolean> constraint = decisionData.getConstraint(branchIndex);
+
+        solverCtx.push();
+
+        try {
+            solverCtx.add(constraint);
+        } catch(RuntimeException ex) {
+            logger.finer(ex.getMessage());
+        }
+        expectedPath.add(branchIndex);
+
+        debugLogger.finest("[extendExpectedPath] extend path, branchIdx=" + branchIndex +
+                        ", constraint=" + constraint.toString());
+    }
+
+    public void constructExpectedPath(Node node) {
+        emptyExpectedPath();
+        Stack<DecisionData> decisionDataStack = new Stack<>();
+        Stack<Integer> indexStack = new Stack<>();
+
+        while (node.getParent() != null) {
+            Node parent = node.getParent();
+            DecisionData decisionData = parent.decisionData();
+
+            if (decisionData != null) {
+                for (int i = 0; i < decisionData.getBranchWidth(); i++) {
+                    if (decisionData.getChild(i) == node) {
+                        decisionDataStack.push(decisionData);
+                        indexStack.push(i);
+                        break;
+                    }
+                }
+            }
+
+            node = parent;
+        }
+
+        while (!decisionDataStack.isEmpty()) {
+            extendExpectedPath(decisionDataStack.pop(), indexStack.pop());
+        }
+    }
+
+    public SolverContextSolveResult solveCurrentPath() {
+        logger.finer("Finding new valuation");
+        Valuation val = new Valuation();
+        ConstraintSolver.Result res = solverCtx.solve(val);
+        logger.finer("Found: " + res + " : " + val);
+        return new SolverContextSolveResult(val, res);
+    }
+}
