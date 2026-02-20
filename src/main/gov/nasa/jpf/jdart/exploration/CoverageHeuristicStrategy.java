de311bcd2d6bdc81553023a7f918a0a250c16e05
SonnyZ789123 <yoran.mertens@telenet.be>
2026-02-20 12:38:27 +0100

    chore: remove logs

--- src/main/gov/nasa/jpf/jdart/exploration/CoverageHeuristicStrategy.java
+++ src/main/gov/nasa/jpf/jdart/exploration/CoverageHeuristicStrategy.java
@@ -175,7 +175,6 @@ public class CoverageHeuristicStrategy implements ExplorationStrategy {
 
             Instruction insn = instructionBranch.getInstruction();
             if (insn == null) { // This is possible for branches created by uncaught exceptions (e.g., div by zero)
-                System.err.println("insn is null for instruction branch: " + instructionBranch);
                 return false;
             }
 
@@ -183,7 +182,6 @@ public class CoverageHeuristicStrategy implements ExplorationStrategy {
             MethodBlockMapCoverage cov = blockMapCoverage.getMethodBlockMapCoverage(mi.getFullName());
 
             if (cov == null) {
-                System.err.println("coverage data not found for method: " + mi.getFullName());
                 return false;
             }
 
@@ -196,7 +194,6 @@ public class CoverageHeuristicStrategy implements ExplorationStrategy {
             }
 
             if (state != BlockCoverageDataDTO.CoverageState.COVERED) {
-                System.err.println("block not covered for instruction: " + insn + " at line " + insn.getLineNumber() + " in method " + mi.getFullName());
                 return false;
             }
 
